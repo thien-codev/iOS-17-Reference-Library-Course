23773ae5e4b0ae72a0b5ad05461dd4440d09dc76
Nguyen Thien <thiennguyen15dt1@gmail.com>
2024-09-20 00:51:02 +0700

    Day 3

---
+++ Day 3/Day3.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// MARK:  Macros -> auto generate code and insert in into our code before the application is compiled
+
+// 1. Free standing macros and attches macros
+
+func showMacros() {
+    print("File name \(#file)")
+    print("File path \(#filePath)")
+}
